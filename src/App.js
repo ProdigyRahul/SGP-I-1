ff47165552256ce9d6edc5c27e15bc26ccbd270a
Rahul Mistry <22cs042@charusat.edu.in>
2023-07-20 13:03:49 +0530

    final Change

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   }, [dispatch]);
 
   return isScreenLocked ? (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+    <motion.div initial={{ opacity: 0  }} animate={{ opacity: 1 }}>
       <LockScreen />
     </motion.div>
   ) : (
